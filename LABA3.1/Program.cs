5c687cd1b97c3526b1fee4035fbddce85d883405
Chloe3c <kristianishenko21@gmail.com>
2024-10-29 21:01:46 +0200

    Добавьте файлы проекта.

---
+++ LABA3.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите верхнюю границу N:");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Нечетные кратные пяти числа от 10 до " + N + ":");
+for (int i = 10; i <= N; i++)
+{
+    if (i % 5 == 0)
+    {
+        if (i % 2 != 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
